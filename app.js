fbe81496e66f6cbdfeca5079fd384ae1b5865181
amitjalui <amitjalui5@gmail.com>
2022-12-05 18:22:09 +0530

    create basic folder structure routes and controllers

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const products_routes = require('./routes/products.js')
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Hi, I am live.');
 });
+
+// middleware to or set router
+app.use('/api/products', products_routes);
 
 const start = async () => {
     try {
