bd173769b8b4d80ed0455f15a0f91094e6790189
amitjalui <amitjalui5@gmail.com>
2022-12-13 19:24:55 +0530

    implement query search

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const Product = require('../model/product.js');
 
 const getAllProducts = async(req, res) => {
-    res.status(200).json({ msg: 'I am getAllProducts' });
+    const productData = await Product.find(req.query);
+    res.status(200).json({ productData });
 }
 
 const getAllProductsTesting = async(req, res) => {
-    res.status(200).json({ msg: 'I am getAllProductsTesting' });
+    const productData = await Product.find(req.query);
+    res.status(200).json({ productData });
 }
 
 module.exports = { getAllProducts, getAllProductsTesting }
