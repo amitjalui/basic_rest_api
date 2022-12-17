a9047fb720b02a14c8677c55accf94d3096f8b3f
amitjalui <amitjalui5@gmail.com>
2022-12-17 13:53:03 +0530

    add query filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,16 @@
 const Product = require('../model/product.js');
 
 const getAllProducts = async(req, res) => {
-    const productData = await Product.find(req.query);
-    res.status(200).json({ productData });
+    const { company, name, feature } = req.query;
+    const queryObj = {};
+
+    if (company) queryObj.company = company;
+    if (feature) queryObj.feature = feature;
+    if (name) queryObj.name = { $regex: name, $options: "i" };
+
+    const productData = await Product.find(queryObj);
+
+    res.status(200).json(productData);
 }
 
 const getAllProductsTesting = async(req, res) => {
--- productsDB.js
+++ productsDB.js
@@ -8,6 +8,7 @@ dotenv.config();
 const start = async () => {
     try {
         await connectDB(process.env.MONGODB_URL);
+        await Product.deleteMany();
         await Product.create(ProductJson);
         console.log("success");
     } catch (error) {
